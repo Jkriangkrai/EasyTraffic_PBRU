63ed91d0d65a396b1bc4714a000f35484dfe4088
unknown <note.kj@hotmail.com>
2015-08-18 11:22:23 +0700

    Befor Break day2

---
+++ app/src/main/java/thailand/pbru/jarirapanya/kriagnkrai/easytraffic/MyAdapter.java
@@ -0,0 +1,47 @@
+package thailand.pbru.jarirapanya.kriagnkrai.easytraffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Lab on 8/18/2015.
+ */
+public class MyAdapter extends BaseAdapter{
+    //explicit
+    private Context objContext;
+    private int[] iconInts;
+    private String[] titleStrings;
+
+    public MyAdapter(Context objContext, int[] iconInts, String[] titleStrings) {
+        this.objContext = objContext;
+        this.iconInts = iconInts;
+        this.titleStrings = titleStrings;
+    }//constructor
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater objLayoutInflater = (LayoutInflater)
+
+
+        return null;
+    }//getView
+}// Main Class
